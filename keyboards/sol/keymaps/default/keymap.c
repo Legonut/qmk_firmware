971e0af6acb1b931b8600304ab12c4e47120d561
BenDover178 <38433419+BenDover178@users.noreply.github.com>
2018-11-30 15:43:35 -0800

    Update keymap.c

--- keyboards/sol/keymaps/default/keymap.c
+++ keyboards/sol/keymaps/default/keymap.c
@@ -114,9 +114,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    *                                    `-------------'  `--------=----'
    */
   [_COLEMAK] = BASE_LAYOUT( \
-      KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,   KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, \
-      KC_A,    KC_R,    KC_S,    KC_T,    KC_G,   KC_K,    KC_N,    KC_E,    KC_I,    KC_O,    \
-      KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,   KC_M,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH  \
+      KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,   KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, \
+      KC_A,    KC_R,    KC_S,    KC_T,    KC_D,   KC_H,    KC_N,    KC_E,    KC_I,    KC_O,    \
+      KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,   KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH  \
   ),
 
 
